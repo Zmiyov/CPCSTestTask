21e585997cad4076d66c8210d357099d75fcd084
Volodymyr <zmiyov88@gmail.com>
2024-06-26 09:13:17 +0300

    Initial Commit

---
+++ CPCSTestTask/CPCSTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPCSTestTaskApp.swift
+//  CPCSTestTask
+//
+//  Created by Volodymyr Pysarenko on 26.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CPCSTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
